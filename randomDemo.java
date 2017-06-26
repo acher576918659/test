800a9d455b8bf8ba92c15ce8d2755ae43464a5da
acher576918659 <asd576918659@gmail.com>
2017-06-26 10:10:19 +0800

    Add files via upload

---
+++ randomDemo.java
@@ -0,0 +1,15 @@
+package tw.aaa.myproject.test;
+//-5~10
+public class randomDemo {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<20;i++)
+		{
+			int num= (int)((Math.random()*17)-6);
+		System.out.print(num+" ");
+		}
+			
+	}
+
+}
